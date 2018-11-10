972ba80e24ec5805a0ebd01f40bb47b9200f72fa
giuseppeM99 <giuseppe.marino1999@gmail.com>
2018-11-10 17:40:41 +0100

    Header

--- bot.php
+++ bot.php
@@ -1,4 +1,13 @@
 <?php
+/*
+Copyright 2018 Giuseppe Marino
+This file is part of GHCleaner.
+GHCleaner is free software: you can redistribute it and/or modify it under the terms of the GNU Affero General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+GHCleaner is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU Affero General Public License for more details.
+You should have received a copy of the GNU General Public License along with GHCleaner.
+If not, see <http://www.gnu.org/licenses/>.
+*/
 declare(ticks = 1);
 function sig_handler($signo, $asd)
 {
